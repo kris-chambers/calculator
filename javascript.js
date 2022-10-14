eeaebe13042d1ee88907e4b6d51b985dfe534171
Kris Chambers <krischambers86@pm.me>
2022-10-14 15:54:37 -0400

    Add constants for all buttons

--- javascript.js
+++ javascript.js
@@ -1,3 +1,21 @@
+const one = document.querySelector('#one');
+const two = document.querySelector('#two');
+const three = document.querySelector('#three');
+const four = document.querySelector('#four');
+const five = document.querySelector('#five');
+const six = document.querySelector('#six');
+const seven = document.querySelector('#seven');
+const eight = document.querySelector('#eight');
+const nine = document.querySelector('#nine');
+const zero = document.querySelector('#zero');
+const decimal = document.querySelector('#decimal');
+const plus = document.querySelector('#plus');
+const minus = document.querySelector('#minus');
+const multiply = document.querySelector('#multipy');
+const divide = document.querySelector('#divide');
+const equal = document.querySelector('#equal');
+const clear = document.querySelector('#clear');
+
 function addition(a, b) {
   return a + b;
 };
